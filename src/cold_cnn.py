2ecce01315f2c46d43faf6d11f680e6cecd0fa9e
Wang Shenghao <shenghao.wang@gojek.com>
2022-11-19 15:54:45 +0800

    Rename the kernel size hyperparameter

--- src/cold_cnn.py
+++ src/cold_cnn.py
@@ -82,28 +82,28 @@ class ColdCNN(torch.nn.Module):
     def __init__(self, hyparams: DictConfig, in_channels: int, seq_len: int):
         super(ColdCNN, self).__init__()
 
-        self.kernel_heights = hyparams.kernel_heights
+        self.kernel_sizes = hyparams.kernel_sizes
 
         agg_seq_len = 0
-        for kernel_height in self.kernel_heights:
-            module_name = f"conv-maxpool-{kernel_height}"
+        for kernel_size in self.kernel_sizes:
+            module_name = f"conv-maxpool-{kernel_size}"
 
             conv_seq_len = self.compute_seq_len(
-                seq_len, kernel_height, hyparams.cnn_stride
+                seq_len, kernel_size, hyparams.cnn_stride
             )
             pooling_seq_len = self.compute_seq_len(
-                conv_seq_len, kernel_height, hyparams.pooling_stride
+                conv_seq_len, kernel_size, hyparams.pooling_stride
             )
             module = torch.nn.Sequential(
                 torch.nn.Conv1d(
                     in_channels=in_channels,
                     out_channels=hyparams.out_channels,
-                    kernel_size=kernel_height,
+                    kernel_size=kernel_size,
                     stride=hyparams.cnn_stride,
                 ),
                 torch.nn.ReLU(),
                 torch.nn.MaxPool1d(
-                    kernel_size=kernel_height, stride=hyparams.pooling_stride
+                    kernel_size=kernel_size, stride=hyparams.pooling_stride
                 ),
             )
 
@@ -120,14 +120,14 @@ class ColdCNN(torch.nn.Module):
         self.activation = torch.nn.Sigmoid()
 
     @staticmethod
-    def compute_seq_len(input_height: int, kernel_height: int, stride: int) -> int:
-        return int((input_height - kernel_height) / stride) + 1
+    def compute_seq_len(input_height: int, kernel_size: int, stride: int) -> int:
+        return int((input_height - kernel_size) / stride) + 1
 
     def forward(self, batch):
 
         conv_maxpool_outputs = [
-            getattr(self, f"conv-maxpool-{kernel_height}")(batch)
-            for kernel_height in self.kernel_heights
+            getattr(self, f"conv-maxpool-{kernel_size}")(batch)
+            for kernel_size in self.kernel_sizes
         ]
         x = torch.cat(conv_maxpool_outputs, axis=2)
         x = self.flatten(x)
